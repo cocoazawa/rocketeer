0015912a476c0d3563c6f2b17e2fe88ad8a0a3fe
Pete Kou <142402417+cocoazawa@users.noreply.github.com>
2023-10-29 03:40:35 +0900

    change rocketeerGUI to support validation layers
    The commit is a step towards finishing the setting up of the project and moving on to graphics rendering (yay!), but isn't done yet (aw...).

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
+// (C) Copyright 2023 @cocoazawa
 #include <rocketeerGUI.hpp>
 using namespace std;
 
+// This is the only set of lines that the code needs to run. 
+// The rest happens in the rocketeerGUI header file. 
 int main() {
     rocketeerApp app;
 
